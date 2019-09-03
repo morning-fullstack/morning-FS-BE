36c89077216f9a9b6e29563ee96d04c9320651a7
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-09-03 10:26:33 -0700

    failing POST note test

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+// const Note = require('../lib/models/Note');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -16,5 +17,22 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+  
+  it('can create a note', () => {
+    return request(app)
+      .post('/api/v1/notes')
+      .send({
+        title: 'this is a title',
+        body: 'this is definitely not a test, never would I ever write a test.'
+      })
+      .then(res => {
+        expect(res.body).toEqual({ 
+          _id: expect.any(String),
+          title: 'this is a title',
+          body: 'this is definitely not a test, never would I ever write a test.',
+          __v: 0
+        });
+      });
   });
 });
